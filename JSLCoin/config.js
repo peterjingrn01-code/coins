21c54cd5d6204873af4647b63ea723a7407acfc4
peterjingrn01-code <peterjingrn01@gmail.com>
2026-07-11 12:03:47 -0400

    Add files via upload

--- JSLCoin/config.js
+++ JSLCoin/config.js
@@ -1,6 +1,1 @@
-window.JSL_CONFIG = Object.freeze({
-  API_BASE: "https://jslcoin-api.peterjingrn01.workers.dev",
-  APP_NAME: "JSLCoin Omega",
-  VERSION: "5.1 Core",
-  OMEGA_URL: "https://omega.jsl-ian.com"
-});
+window.JSL_CONFIG=Object.freeze({API_BASE:"https://jslcoin-api.peterjingrn01.workers.dev",VERSION:"6.0 Clean"});
